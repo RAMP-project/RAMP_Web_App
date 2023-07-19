c4b86c32c77648f4bd1faea641ac2a01dcdf83d7
Sarah Eckhoff <eckhoffwi.uni-hannover.de>
2023-07-19 13:47:56 +0200

    use pypi rampdemand (newest RAMP version) and fix styling bug; there is a bug in the calculation!

--- ramp_app/main/run_ramp.py
+++ ramp_app/main/run_ramp.py
@@ -1,12 +1,45 @@
 import numpy as np
-from .RAMPedited.ramp.core.stochastic_process import Stochastic_Process
+from ramp import User,calc_peak_time_range,yearly_pattern
+from datetime import datetime
 
 def run_ramp(list_of_appliances):
-    profile = Stochastic_Process(appliances_list=list_of_appliances)
-    # convert from 365 day profiles in minutes to one hourly profile
-    yearly_profile_in_minutes = np.zeros(0)
-    for prof in profile:
-        yearly_profile_in_minutes = np.append(yearly_profile_in_minutes, prof)
+    # Create a user category
+    myuser = User(
+        num_users=1,  # Specifying the number of specific user category in the community
+    )
+
+    for app in list_of_appliances:
+        window_1_start_hours = datetime.strptime(app['window_1_start'], '%H:%M')
+        window_1_start_minutes = int(window_1_start_hours.hour * 60 + window_1_start_hours.minute)
+        window_1_end_hours = datetime.strptime(app['window_1_end'], '%H:%M')
+        window_1_end_minutes = int(window_1_end_hours.hour * 60 + window_1_end_hours.minute)
+        window_2_start_hours = datetime.strptime(app['window_2_start'], '%H:%M')
+        window_2_start_minutes = int(window_2_start_hours.hour * 60 + window_2_start_hours.minute)
+        window_2_end_hours = datetime.strptime(app['window_2_end'], '%H:%M')
+        window_2_end_minutes = int(window_2_end_hours.hour * 60 + window_2_end_hours.minute)
+        window_3_start_hours = datetime.strptime(app['window_3_start'], '%H:%M')
+        window_3_start_minutes = int(window_3_start_hours.hour * 60 + window_3_start_hours.minute)
+        window_3_end_hours = datetime.strptime(app['window_3_end'], '%H:%M')
+        window_3_end_minutes = int(window_3_end_hours.hour * 60 + window_3_end_hours.minute)
+
+        a = myuser.add_appliance(number=app['number'], power=app['P'], num_windows=app['num_windows'], func_time=app['func_time'],
+                             time_fraction_random_variability=app['r_t']/100, func_cycle=app['func_cycle'], occasional_use=app['occasional_use']/100,
+                             wd_we_type=app['wd_we'])
+        a.windows(window_1=[window_1_start_minutes, window_1_end_minutes], window_2=[window_2_start_minutes, window_2_end_minutes],
+                  random_var_w=app['r_w']/100, window_3=[window_3_start_minutes, window_3_end_minutes])
+
+    whole_year_profile = []
+    Year_behaviour = yearly_pattern()
+    for i in range(365):
+        whole_year_profile.extend(
+            myuser.generate_single_load_profile(
+                prof_i=i,
+                peak_time_range=calc_peak_time_range(user_list=[myuser]),
+                day_type=Year_behaviour[i],
+            )
+        )
+    # convert from minutes and W to hours and kW profile
+    yearly_profile_in_minutes = np.array(whole_year_profile)
     yearly_profile_in_hours = np.zeros(8760)
     for i in range(8760):
         yearly_profile_in_hours[i] = sum(yearly_profile_in_minutes[i*60:(i+1)*60-1]) / 60
